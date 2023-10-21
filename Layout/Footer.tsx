1c57415de35d1d95f58b001f8f6b1d9c169f1abc
Loren Cerri <lorencerri@gmail.com>
2023-10-20 22:47:47 -0400

    Update Footer.tsx

--- Layout/Footer.tsx
+++ Layout/Footer.tsx
@@ -1,6 +1,8 @@
 import classes from "./Footer.module.css";
-import { Anchor, Group, Title } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Anchor, Group, Modal, Title, Button } from "@mantine/core";
 import type { ReactNode } from "react";
+import DoomGUI from "../DOOM/DoomGUI";
 
 interface FooterProps {
   logo: ReactNode;
@@ -8,10 +10,22 @@ interface FooterProps {
 }
 
 export const Footer = ({ logo, links }: FooterProps) => {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <Group className={classes.footer}>
       <Title order={2} className={classes.title}>
-        {logo}
+        <Modal
+          size="xl"
+          opened={opened}
+          onClose={close}
+          title="Is this an easter egg?"
+        >
+          <DoomGUI />
+        </Modal>
+        <Button variant="light" color="red" radius="md" onClick={open}>
+          Don't Click Me
+        </Button>
       </Title>
       {links && (
         <Group className={classes.links}>
